bb8022af4b59c27aa32e97039118325f1b97da3b
Carpa88 <olenka_311@mail.ru>
2023-02-16 10:45:44 +0300

    add react, mui and setting with them

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,85 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const plugins = [
+	new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css',
+	}), 
+	new HtmlWebpackPlugin({
+	//   template: './src/index.html',
+		title: 'Portfolio',
+})
+]
+
+if (process.env.SERVE) { // Используем плагин только если запускаем devServer
+  plugins.push(new ReactRefreshWebpackPlugin());
+} // Данный код должен быть размещен после объявления массива plugins
+
+
+let mode = 'development'; 
+let target = 'web';
+if (process.env.NODE_ENV === 'production') { 
+  mode = 'production';
+  target = 'browserslist';
+}
 
 module.exports = {
-  mode: 'development',
+  mode,
+  target,
   entry: './src/index.js',
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type:  mode === 'production' ? 'asset' : 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      { test: /\.(html)$/, use: ['html-loader'] },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.css$/i, 
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+			{
+        test: /\.jsx?$/, // обновляем регулярное выражение для поддержки jsx
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // не обрабатываем файлы из node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+            // при каждом запуске
+          },
+        },
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins: [
-    new MiniCssExtractPlugin(), 
-    new HtmlWebpackPlugin({
-    title: 'Portfolio',
-  }),],
+  plugins,
   devServer: {
     static: './dist',
     hot: true,
@@ -42,6 +87,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
     publicPath: '/',
   },
